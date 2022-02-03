003dc37dac49a92f2df17a316e4ffd0a22d1830d
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-03 22:12:41 +0545

    feat: update layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { ReactHTMLElement } from "react";
+
+function Layout({ children, className, ...props }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <section className={`Layout ${className}`} {...props}>
+      <header>header</header>
+      <main>{children}</main>
+      <footer>footer</footer>
+    </section>
+  );
+}
+
+export default Layout;
