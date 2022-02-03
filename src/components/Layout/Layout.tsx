ca754dc86efcd11a0d871516ffe85b4055d09628
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-03 22:16:06 +0545

    refactor

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,10 @@ import React from "react";
 function Layout({ children, className, ...props }: React.HTMLAttributes<HTMLElement>) {
   return (
     <section className={`Layout ${className}`} {...props}>
-      <header>header</header>
+      <header>
+        <nav>Nav</nav>
+        header
+      </header>
       <main>{children}</main>
       <footer>footer</footer>
     </section>
